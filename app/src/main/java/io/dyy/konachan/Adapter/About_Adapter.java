173d99709907000e833c85eb8e7edbc013a83066
dyy <i.daoyy@gmail.com>
2017-08-03 18:37:28 +0800

    大致上完成了

---
+++ app/src/main/java/io/dyy/konachan/Adapter/About_Adapter.java
@@ -0,0 +1,8 @@
+package io.dyy.konachan.Adapter;
+
+/**
+ * Created by dyy on 2017/8/3.
+ */
+
+public class About_Adapter {
+}
---
+++ app/src/main/java/io/dyy/konachan/data/LibrariesData.java
@@ -0,0 +1,8 @@
+package io.dyy.konachan.data;
+
+/**
+ * Created by dyy on 2017/8/3.
+ */
+
+public class LibrariesData {
+}
